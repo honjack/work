72cee9629aa2effb3e732d3d17838dd779118fcd
Honjack <honjack@gmail.com>
2017-09-15 16:54:10 +0800

    First

--- database/migrations/2017_09_15_080523_create_clues_companies_table.php
+++ database/migrations/2017_09_15_080523_create_clues_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCluesCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('clues_companies', function (Blueprint $table) {
+            $table->integer('clueId');
+            $table->integer('companyId');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('clues_companies');
     }
 }
