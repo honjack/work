7c41ea1123bf57908223bc31c7cbf8d16d3a0428
Honjack <honjack@gmail.com>
2017-10-09 17:19:20 +0800

    First

--- resources/views/bureau/create.blade.php
+++ resources/views/bureau/create.blade.php
@@ -1,3 +1,14 @@
-{!! Form::open(['route' => 'bureau.create']) !!}
-   Form::text('bureau');
-{!! Form::close() !!}
+
+<div class="form-group">
+    {!! Form::open(array('route'=>'bureau.store')) !!}
+        <div class="form-control">
+            {!! Form::label('bureau','bureau:') !!}
+            {!! Form::text('bureau',null,['class'=>'form-control','autofocus'=>'autofocus']) !!}
+        </div>
+        <div class="form-control">
+            {!! Form::submit('创建',['class'=>'btn btn-primary form-control']) !!}
+        </div>
+{{Form::close()}}
+</div>
+
+
